e174b90b2b8020f463038637a48675ff82ce5e25
Ersado223307039 <ersadocb2505@gmail.com>
2024-04-03 11:49:26 +0700

    commitment,foundersosmed,sosmed,HIW

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -19,7 +19,6 @@ $routes->get('/en/destination', 'DestinationController::index');
 $routes->get('/en/destination/(:segment)', 'DestinationController::detail/$1');
 
 $routes->get('/contact-us', 'ContactUsController::index');
-$routes->get('/dashboard', 'admin\Dashboardctrl::index');
 $routes->get('/about-us', 'AboutUsController::index');
 
 $routes->get('/id/faq', 'FAQController::index');
@@ -37,3 +36,32 @@ $routes->get('/gallery/detail', 'GalleryController::detail');
 $routes->set404Override('App\Controllers\ErrorController::show404');
 
 //admin
+$routes->get('/dasboard', 'admin\Dashboardctrl::index');
+
+$routes->get('/commitment', 'admin\Commitment::index');
+$routes->get('/tambahCommitment', 'admin\Commitment::tambah');
+$routes->post('/proses_tambah', 'admin\Commitment::proses_tambah');
+$routes->get('/editCommitment/(:num)', 'admin\Commitment::edit/$1');
+$routes->post('/proses_edit/(:num)', 'admin\Commitment::proses_edit/$1');
+$routes->get('/delete/(:any)', 'admin\Commitment::delete/$1');
+
+$routes->get('/FounderSosmed', 'admin\FounderSosmedctrl::index');
+$routes->get('/tambahFounderSosmed', 'admin\FounderSosmedctrl::tambah');
+$routes->post('/proses_tambah_FounderSosmed', 'admin\FounderSosmedctrl::proses_tambah');
+$routes->get('/editFounderSosmed/(:num)', 'admin\FounderSosmedctrl::edit/$1');
+$routes->post('/proses_edit_FounderSosmed/(:num)', 'admin\FounderSosmedctrl::proses_edit/$1');
+$routes->get('/delete_FounderSosmed/(:any)', 'admin\FounderSosmedctrl::delete/$1');
+
+$routes->get('/how_it_works', 'admin\HowItWorkctrl::index');
+$routes->get('/tambahhow_it_works', 'admin\HowItWorkctrl::tambah');
+$routes->post('/proses_tambah_how_it_works', 'admin\HowItWorkctrl::proses_tambah');
+$routes->get('/edithow_it_works/(:num)', 'admin\HowItWorkctrl::edit/$1');
+$routes->post('/proses_edit_how_it_works/(:num)', 'admin\HowItWorkctrl::proses_edit/$1');
+$routes->get('/delete_how_it_works/(:any)', 'admin\HowItWorkctrl::delete/$1');
+
+$routes->get('/SosialMedia', 'admin\SosialMediactrl::index');
+$routes->get('/tambahSosialMedia', 'admin\SosialMediactrl::tambah');
+$routes->post('/proses_tambah_SosialMedia', 'admin\SosialMediactrl::proses_tambah');
+$routes->get('/editSosialMedia/(:num)', 'admin\SosialMediactrl::edit/$1');
+$routes->post('/proses_edit_SosialMedia/(:num)', 'admin\SosialMediactrl::proses_edit/$1');
+$routes->get('/delete_SosialMedia/(:any)', 'admin\SosialMediactrl::delete/$1');
--- app/Controllers/admin/Dashboardctrl.php
+++ app/Controllers/admin/Dashboardctrl.php
@@ -2,15 +2,18 @@
 
 namespace App\Controllers\admin;
 
-use App\Controllers\admin\BaseController;
+use App\Controllers\BaseController;
 
 class Dashboardctrl extends BaseController
 {
     public function index(): void
     {
+       $data['title'] = 'Contact Us – Astrip';
+        $data['language'] = 'id';
         // $data['title'] = 'Contact Us – Astrip';
         // $data['language'] = 'id';
 
+        echo view('pages/admin/dasboard', $data);
         // echo view('pages/dasboard', $data);
 
         echo "dashboard admin";
